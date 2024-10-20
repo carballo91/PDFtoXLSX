161220efaa755f3f408f88bc540b1a8956671184
carballo91 <carballo.rafael91@gmail.com>
2024-10-19 20:50:34 -0400

    Update urls.py

--- pdf_to_excel/urls.py
+++ pdf_to_excel/urls.py
@@ -5,4 +5,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.upload_pdf, name='upload_pdf'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('download_file/', views.download_file, name='download_file'),]
