41c782ad587fa2d44dff44751ead8b45c584b4ae
carballo91 <carballo.rafael91@gmail.com>
2024-10-23 00:54:17 -0400

    Update forms.py

--- pdf_to_excel/forms.py
+++ pdf_to_excel/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class PDFUploadForm(forms.Form):
-    pdf_file = forms.FileField()
+    pdf_file = forms.FileField(widget=forms.FileInput(attrs={"id":"pdf_file","allow_multiple_selected": True}))
