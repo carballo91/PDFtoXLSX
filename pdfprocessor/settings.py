2defd981f5f8dac9c46811d71c6c9ab6108a9389
carballo91 <carballo.rafael91@gmail.com>
2024-11-06 13:34:44 -0500

    Update settings.py

--- pdfprocessor/settings.py
+++ pdfprocessor/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pdf_to_excel',
+    'csp',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
+CSP_FRAME_ANCESTORS = ["*"]
 
 ROOT_URLCONF = 'pdfprocessor.urls'
 
