8ea7ad360840b917bc76e055f7c08db11f35d88e
carballo91 <carballo.rafael91@gmail.com>
2024-11-11 19:59:39 -0500

    Update views.py

--- pdf_to_excel/views.py
+++ pdf_to_excel/views.py
@@ -1,20 +1,18 @@
 import os
-import pdfplumber
-import pandas as pd
-import re
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .forms import PDFUploadForm
 from django.urls import reverse
-from io import BytesIO
 from .helpers import PDFEditor
+import urllib.parse
+from django.urls import reverse
+
 
 def upload_pdf(request):
     if request.method == "POST":
         form = PDFUploadForm(request.POST, request.FILES)
         if form.is_valid():
             pdf_files = request.FILES.getlist('pdf_file')  # Get the list of uploaded files
-            download_urls = []  # To store download URLs
             filenames = []
 
             for pdf_file in pdf_files:
@@ -57,25 +55,28 @@ def upload_pdf(request):
 
                 # Save the DataFrame to Excel and get the file path
                 filename = pdf_editor.save_to_excel(df, output_name)
-
-                # Create the download link
-                download_url = reverse("download_file", args=[output_name])
-                download_urls.append(download_url)
                 filenames.append(filename)
-
-            download_data = zip(download_urls, filenames)
-            return render(request, 'upload.html', {'download_data': download_data, 'form': form})
+     
+            if len(filenames) > 1: # Create the zip file containing all the Excel files 
+   
+                download_url = reverse("download_file", args=["processed_pdfs.zip"])
+                name = download_url.split("/")[2]
+                print(name)
+            else: # Single file, do not create a zip 
+                single_file_name = os.path.basename(filenames[0]) 
+                download_url = reverse("download_file", args=[urllib.parse.quote(single_file_name)])
+                name = download_url.split("/")[2]
+                print(name)
+            return render(request, 'upload.html', {'download_url': download_url, 'form': form,"name":name,})
 
     else:
         form = PDFUploadForm()
 
     return render(request, 'upload.html', {'form': form})
 
-from django.http import FileResponse
-
-def download_file(request,filename):
-    file_path = os.path.join("/tmp",f"{filename}.xlsx")
+def download_file(request, filename):
+    file_path = os.path.join("/tmp", urllib.parse.unquote(filename))
     if os.path.exists(file_path):
-        return FileResponse(open(file_path, 'rb'), as_attachment=True, filename=f'{filename}.xlsx')
+        return FileResponse(open(file_path, 'rb'), as_attachment=True, filename=filename)
     else:
         return HttpResponse("File not found", status=404)
