40a06237bb143ebd15557b97e7f6963379b798ad
carballo91 <carballo.rafael91@gmail.com>
2025-01-15 20:29:24 -0500

    Update helpers.py

--- pdf_to_excel/helpers.py
+++ pdf_to_excel/helpers.py
@@ -830,7 +830,7 @@ class PDFEditor:
         for table in tables:
             if len(table) > 2 and len(table[1]) == 1 or len(table[2]) == 1:
                 selling_agent = re.match(selling_agent_pattern,table[1][0])
-                if not selling_agent:
+                if selling_agent is None:
                     selling_agent = re.match(selling_agent_pattern,table[2][0])
                 agent_no,agency = selling_agent.groups() 
                 info = [x for x in table[3:] if x[0] != "" and x[1] != ""]
