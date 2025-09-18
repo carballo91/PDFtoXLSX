f0651c47aecfbb714ab2f155ac7cf610af3a1773
carballo91 <carballo.rafael91@gmail.com>
2025-09-18 18:05:46 -0400

    Add files via upload

--- pdf_to_excel/forms.py
+++ pdf_to_excel/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class PDFUploadForm(forms.Form):
     pdf_file = forms.FileField(widget=forms.FileInput(attrs={"id":"pdf_file","allow_multiple_selected": True}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"id":"pw","placeholder":"Enter PDF Password"}),required=False,label="Enter PDF Password")
